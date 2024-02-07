90f34ba2409c7327b94c9bef527282970fdb69eb
Shuqiang Zhang <sqzhang@meta.com>
2024-02-06 20:16:12 -0800

    [c10d] explicitly abort communicators in destroy_process_group call Summary: This PR tries to resolve issue #119215.
    Basically,  processgroup shutdown (and hence ncclCommAbort) is called in
    destroy_process_group APIs for the corresponding PGs. and in the
    destructor of ProcessGroup, we avoid calling abort/ncclCommAbort.
    Instead, it just checks if the users have explicitly already called destroy_process_group. If
    not, Destructor will log a warning and encourage/expect users to do so
    for cleanup of resources of PGs.
    
    ghstack-source-id: 9a75d181a096c250828eb06acf70e167e26cb05c
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/119250

--- test/distributed/test_c10d_nccl.py
+++ test/distributed/test_c10d_nccl.py
@@ -1196,6 +1196,29 @@ class ProcessGroupNCCLTest(MultiProcessTestCase):
         with self.assertRaises(dist.DistBackendError):
             pg.allreduce([t])
 
+
+    @requires_nccl()
+    @skip_but_pass_in_sandcastle_if(not TEST_MULTIGPU, "NCCL test requires 2+ GPUs")
+    def test_abort_in_destroy_pg(self):
+        # Disable ASYNC_ERROR_HANDLING for this test to ensure we can programmatically
+        # abort the process group.
+        os.environ["TORCH_NCCL_ASYNC_ERROR_HANDLING"] = "0"
+        os.environ["TORCH_NCCL_ABORT_IN_DESTROY_PG"] = "1"
+
+        store = c10d.FileStore(self.file_name, self.world_size)
+        pg = self._create_process_group_nccl(store, self.opts())
+        device = self.rank_to_GPU[self.rank][0]
+
+        t = torch.rand(10, 10, device=device)
+        # First allreduce to initialize state.
+        pg.allreduce(t)
+
+        # Destroy pg and validate pg is NOT in working condition since
+        # we have shutdown comms
+        dist.destroy_process_group()
+        with self.assertRaises(dist.DistBackendError):
+            pg.allreduce([t])
+
     @requires_nccl()
     @skip_but_pass_in_sandcastle_if(torch.cuda.device_count() < 2, "NCCL test requires 2+ GPUs")
     def test_close_multi_pg_unordered(self):
@@ -1224,6 +1247,28 @@ class ProcessGroupNCCLTest(MultiProcessTestCase):
             del new_pg1
             dist.destroy_process_group(new_pg2)
             del new_pg2
+        dist.destroy_process_group()
+
+    @requires_nccl()
+    @skip_but_pass_in_sandcastle_if(torch.cuda.device_count() < 2, "NCCL test requires 2+ GPUs")
+    def test_abort_in_destroy_multi_pgs(self):
+        os.environ["TORCH_NCCL_ABORT_IN_DESTROY_PG"] = "1"
+        store = c10d.FileStore(self.file_name, self.world_size)
+        pg = self._create_process_group_nccl(store, self.opts())
+        device = self.rank_to_GPU[self.rank][0]
+        t = torch.rand(10, 10, device=device)
+        # First allreduce to initialize default PG's communicator.
+        pg.allreduce(t).wait()
+        new_pg1 = c10d.new_group([0, 1])
+        new_pg2 = c10d.new_group([0, 1])
+        t1 = torch.rand(10, 10, device=device)
+        t2 = torch.rand(10, 10, device=device)
+        new_pg1.allreduce(t1).wait()
+        new_pg2.allreduce(t2).wait()
+        backend = pg._get_backend(torch.device(device))
+        # default PG's backend should have a split count of 2
+        self.assertEqual(backend.comm_split_count(), 2)
+        # shutdown all NCCL PGs in one shot
         dist.destroy_process_group()
 
     @requires_nccl()
